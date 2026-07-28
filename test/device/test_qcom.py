79bf35083b639392932dc12182f762896093b9e0
Andrei Radulescu <andi.radulescu@gmail.com>
2026-07-28 16:18:15 +0300

    qcom: add MSM DRM UAPI

--- test/device/test_qcom.py
+++ test/device/test_qcom.py
@@ -1,6 +1,33 @@
 import ctypes, platform, unittest
 from tinygrad import Device
 from tinygrad.renderer.cstyle import ClangRenderer
+from tinygrad.runtime.autogen import msm_drm
+
+def ioctl_number(ioctl):
+  direction, base, number, struct_type = ioctl.args
+  return direction << 30 | ctypes.sizeof(struct_type) << 16 | base << 8 | number
+
+class TestMSMDRMUAPI(unittest.TestCase):
+  def test_struct_layouts_and_ioctl_numbers(self):
+    layouts = {
+      msm_drm.struct_drm_msm_param: (24, (0, 4, 8, 16, 20)),
+      msm_drm.struct_drm_msm_gem_new: (16, (0, 8, 12)),
+      msm_drm.struct_drm_msm_gem_info: (24, (0, 4, 8, 16, 20)),
+      msm_drm.struct_drm_msm_gem_submit_cmd: (32, (0, 4, 8, 12, 16, 20, 24, 24)),
+      msm_drm.struct_drm_msm_gem_submit_bo: (16, (0, 4, 8)),
+      msm_drm.struct_drm_msm_gem_submit: (72, (0, 4, 8, 12, 16, 24, 32, 36, 40, 48, 56, 60, 64, 68)),
+      msm_drm.struct_drm_msm_wait_fence: (32, (0, 4, 8, 24)),
+      msm_drm.struct_drm_msm_submitqueue: (12, (0, 4, 8)),
+    }
+    for struct_type, (size, offsets) in layouts.items():
+      with self.subTest(struct=struct_type.__name__):
+        self.assertEqual(ctypes.sizeof(struct_type), size)
+        self.assertEqual(tuple(field[2] for field in struct_type._real_fields_), offsets)
+
+    self.assertEqual(ioctl_number(msm_drm.DRM_IOCTL_GEM_CLOSE), 0x40086409)
+    self.assertEqual(ioctl_number(msm_drm.DRM_IOCTL_MSM_GET_PARAM), 0xC0186440)
+    self.assertEqual(ioctl_number(msm_drm.DRM_IOCTL_MSM_GEM_SUBMIT), 0xC0486446)
+    self.assertEqual(ioctl_number(msm_drm.DRM_IOCTL_MSM_WAIT_FENCE), 0x40206447)
 
 class TestQCOM(unittest.TestCase):
   # although part of the QCOM runtime, this tests flushing the CPU's dcache
