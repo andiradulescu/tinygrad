fb9a07ac4a90175ebb1870337b55cc38b0a6183f
Andrei Radulescu <andi.radulescu@gmail.com>
2026-08-24 00:12:59 +0300

    qcom: support MSM DRM on Adreno 630
    ORANGE: AI was used for implementation changes, tests, and review.

--- test/null/test_qcom.py
+++ test/null/test_qcom.py
@@ -1,5 +1,5 @@
-import ctypes, sys, unittest
-from unittest.mock import Mock
+import ctypes, mmap, sys, unittest
+from unittest.mock import Mock, patch
 from tinygrad.runtime.autogen import msm_drm
 from tinygrad.runtime.support.hcq import HCQBuffer, MMIOInterface
 
@@ -26,27 +26,62 @@ class TestMSMDRMUAPI(unittest.TestCase):
 @unittest.skipIf(sys.platform == "win32", "QCOM is not supported on Windows")
 class TestQCOMCommandBuffer(unittest.TestCase):
   def test_cpu_view(self):
-    from tinygrad.runtime.ops_qcom import QCOMComputeQueue
+    from tinygrad.runtime.ops_qcom import KGSLIface, QCOMComputeQueue
     gpu_mem = (ctypes.c_uint32 * 2)(0xaaaaaaaa, 0xaaaaaaaa)
     cpu_mem = (ctypes.c_uint32 * 2)()
     cmd_buf = HCQBuffer(ctypes.addressof(gpu_mem), 8, view=MMIOInterface(ctypes.addressof(cpu_mem), 8))
-    dev = Mock(ctx=0, cmd_buf=cmd_buf)
+    iface, dev = object.__new__(KGSLIface), Mock(ctx=0, cmd_buf=cmd_buf)
+    iface.ctx, dev.iface = 0, iface
+    dev.cmd_buf_allocator.alloc.return_value = cmd_buf.va_addr + 4
     queue = QCOMComputeQueue(dev)
     queue.q(0x12345678)
 
-    dev.cmd_buf_allocator.alloc.return_value = cmd_buf.va_addr + 4
-    _, obj = queue._build_gpu_command(dev)
+    page = queue._build_gpu_command(dev)
+    _, obj = iface.prepare_submit(page, 4, set())
     self.assertEqual(obj.gpuaddr, cmd_buf.va_addr + 4)
-    self.assertEqual(tuple(gpu_mem), (0xaaaaaaaa, 0xaaaaaaaa))
-    self.assertEqual(tuple(cpu_mem), (0, 0x12345678))
+    self.assertEqual((tuple(gpu_mem), tuple(cpu_mem)), ((0xaaaaaaaa, 0xaaaaaaaa), (0, 0x12345678)))
 
     dev.allocator.alloc.return_value = cmd_buf.offset(size=4)
     queue.bind(dev)
-    self.assertEqual(queue.obj.gpuaddr, cmd_buf.va_addr)
-    self.assertEqual(tuple(gpu_mem), (0xaaaaaaaa, 0xaaaaaaaa))
-    self.assertEqual(tuple(cpu_mem), (0x12345678, 0x12345678))
+    self.assertEqual(queue.hw_page.va_addr, cmd_buf.va_addr)
+    self.assertEqual((tuple(gpu_mem), tuple(cpu_mem)), ((0xaaaaaaaa, 0xaaaaaaaa), (0x12345678, 0x12345678)))
     queue._q[0] = 0x87654321
-    self.assertEqual(tuple(cpu_mem), (0x87654321, 0x12345678))
+    self.assertEqual((tuple(gpu_mem), tuple(cpu_mem)), ((0xaaaaaaaa, 0xaaaaaaaa), (0x87654321, 0x12345678)))
+
+@unittest.skipIf(sys.platform == "win32", "QCOM is not supported on Windows")
+class TestMSMInterface(unittest.TestCase):
+  def test_allocation_and_submit(self):
+    from tinygrad.runtime.ops_qcom import MSMAllocation, MSMIface
+    memory = [(ctypes.c_ubyte * mmap.PAGESIZE)() for _ in range(2)]
+    fd = Mock()
+    fd.mmap.side_effect, fd.munmap.return_value = [ctypes.addressof(x) for x in memory], 0
+    iface = object.__new__(MSMIface)
+    iface.dev, iface.fd, iface.allocations = Mock(error_state=None), fd, {}
+    iovas = {7:0x10000000, 9:0x20000000}
+
+    def gem_info(_fd, handle, info):
+      return Mock(value=iovas[handle] if info == msm_drm.MSM_INFO_GET_IOVA else handle * mmap.PAGESIZE)
+
+    with (
+      patch.object(msm_drm, 'DRM_IOCTL_MSM_GEM_NEW', side_effect=[Mock(handle=7), Mock(handle=9)]) as gem_new,
+      patch.object(msm_drm, 'DRM_IOCTL_MSM_GEM_INFO', side_effect=gem_info),
+    ):
+      command, data = iface.alloc(17), iface.alloc(32)
+
+    self.assertIsInstance(command.meta, MSMAllocation)
+    self.assertEqual((command.meta.size, command.meta.mapped_size), (17, mmap.PAGESIZE))
+    self.assertEqual([call.kwargs['flags'] for call in gem_new.call_args_list], [msm_drm.MSM_BO_WC, msm_drm.MSM_BO_WC])
+
+    buffers = {data, data.offset(4, 8), HCQBuffer(data.va_addr, data.size)}
+    submit, bos, cmds = iface.prepare_submit(command.offset(4, 8), 8, buffers)
+    read_write = msm_drm.MSM_SUBMIT_BO_READ | msm_drm.MSM_SUBMIT_BO_WRITE
+    self.assertEqual((submit.nr_bos, submit.queueid), (2, 0))
+    self.assertEqual([(bo.flags, bo.handle, bo.presumed) for bo in bos], [
+      (msm_drm.MSM_SUBMIT_BO_READ, 7, 0x10000000),
+      (read_write, 9, 0x20000000),
+    ])
+    self.assertEqual((cmds[0].submit_idx, cmds[0].submit_offset, cmds[0].size), (0, 4, 8))
+    with self.assertRaisesRegex(ValueError, "outside its buffer"): iface.prepare_submit(command.offset(16, 4), 4, set())
 
 if __name__ == '__main__':
   unittest.main()
