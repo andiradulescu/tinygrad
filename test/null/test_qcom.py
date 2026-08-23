b91336a3bae3a6f434c212a0a7e8137242a824f3
Andrei Radulescu <andi.radulescu@gmail.com>
2026-08-24 00:12:43 +0300

    qcom: add MSM DRM UAPI

--- test/null/test_qcom.py
+++ test/null/test_qcom.py
@@ -1,6 +1,27 @@
 import ctypes, sys, unittest
 from unittest.mock import Mock
+from tinygrad.runtime.autogen import msm_drm
 from tinygrad.runtime.support.hcq import HCQBuffer, MMIOInterface
+
+def ioctl_number(ioctl):
+  direction, base, number, struct_type = ioctl.args
+  return direction << 30 | ctypes.sizeof(struct_type) << 16 | base << 8 | number
+
+class TestMSMDRMUAPI(unittest.TestCase):
+  def test_layouts(self):
+    layouts = {
+      msm_drm.struct_drm_msm_param: (24, (0, 4, 8, 16, 20)),
+      msm_drm.struct_drm_msm_gem_new: (16, (0, 8, 12)),
+      msm_drm.struct_drm_msm_gem_info: (24, (0, 4, 8, 16, 20)),
+      msm_drm.struct_drm_msm_gem_submit_cmd: (32, (0, 4, 8, 12, 16, 20, 24, 24)),
+      msm_drm.struct_drm_msm_gem_submit_bo: (16, (0, 4, 8)),
+      msm_drm.struct_drm_msm_gem_submit: (72, (0, 4, 8, 12, 16, 24, 32, 36, 40, 48, 56, 60, 64, 68)),
+    }
+    for struct_type, (size, offsets) in layouts.items():
+      self.assertEqual((ctypes.sizeof(struct_type), tuple(x[2] for x in struct_type._real_fields_)), (size, offsets))
+    self.assertEqual(ioctl_number(msm_drm.DRM_IOCTL_GEM_CLOSE), 0x40086409)
+    self.assertEqual(ioctl_number(msm_drm.DRM_IOCTL_MSM_GET_PARAM), 0xC0186440)
+    self.assertEqual(ioctl_number(msm_drm.DRM_IOCTL_MSM_GEM_SUBMIT), 0xC0486446)
 
 @unittest.skipIf(sys.platform == "win32", "QCOM is not supported on Windows")
 class TestQCOMCommandBuffer(unittest.TestCase):
